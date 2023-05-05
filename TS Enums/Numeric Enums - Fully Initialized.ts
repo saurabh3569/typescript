75e9f5a42f133b6369a6cac77090747d45749497
saurabh3569 <smishra9109@gmail.com>
2023-05-05 15:03:25 +0530

    TypeScript Enums

---
+++ TS Enums/Numeric Enums - Fully Initialized.ts
@@ -0,0 +1,8 @@
+enum StatusCodes {
+  NotFound = 404,
+  Success = 200,
+  Accepted = 202,
+  BadRequest = 400,
+}
+console.log(StatusCodes);
+// 404 200 202 400
---
+++ TS Enums/Numeric Enums - Initialized.ts
@@ -0,0 +1,8 @@
+enum CardinalDirections1 {
+  North = 1,
+  East,
+  South,
+  West,
+}
+console.log(CardinalDirections1);
+// 1 2 3 4
